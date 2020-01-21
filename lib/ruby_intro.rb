d63ff283da06d2988957f9de397c48ab7dd536d6
EC2 Default User <ec2-user@ip-172-31-22-204.ec2.internal>
2020-01-21 18:26:00 +0000

    finish 2,3,4

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,21 +3,45 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  i = 0;
+  arr.each { |item|
+    i = i + item
+  }
+  return i
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  # if ( arr.length() != 0 ) 
+  #   sum =  arr.max
+  #   i = arr.index(sum)
+  #   arr.slice!(i)
+  #   sum = sum + arr.max
+  # elsif (arr.length() == 1 )
+  #   sum = arr[0]
+  # else 
+  #   sum = 0
+  # end
+  # return sum
+  
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  
+  arr.each_with_index {|element, i|
+    arr.each_with_index {|item, j |
+      if (element + item === n and i != j ) 
+        return true
+      end
+    }
+  }
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  nm = "hello, "+name
+  return nm
 end
 
 def starts_with_consonant? s
@@ -33,3 +57,11 @@ end
 class BookInStock
 # YOUR CODE HERE
 end
+
+# nums = [1,2,3,4]
+# n =  sum_to_n? nums,2
+# puts n
+
+puts hello "madeleine"
+
+
